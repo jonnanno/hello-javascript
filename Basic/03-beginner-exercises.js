9cc895c0199025b9218ddc664d7b6a9c8e19b0f1
jonnanno <jhonmarchi@gmail.com>
2024-07-20 09:38:31 -0400

    primer ejercicio realizado

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -6,20 +6,100 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Escribe un comentario en una línea
 
+    // este es un comentario a una sola linea - JM
+
+
 // 2. Escribe un comentario en varias líneas
+
+
+    /*  Este es un 
+        comentario a 
+        varias lineas  */
+
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+    let greeting = "hola, "
+    let myName = "Jhonnatan"
+
+    let myAge = 42
+    let myHeight = 1.70
+
+    let isLearning = true
+
+    let undefinedVar = undefined
+    let nullVar = null
+
+    let mySymbol = Symbol("mySymbol")
+
+    let myBigNumber = BigInt(55555555555555555555555555555555555555555555555555555555555)
+
+
 // 4. Imprime por consola el valor de todas las variables
+
+    console.log(greeting + myName)
+    console.log("mi edad es " + myAge + " y mi estatura es: " + myHeight)
+    console.log(isLearning)
+
+    
+
+    console.log(undefinedVar)
+    console.log(nullVar)
+
+    console.log(mySymbol)
+    console.log(myBigNumber)
+
 
 // 5. Imprime por consola el tipo de todas las variables
 
+    console.log(typeof greeting)
+    console.log(typeof myName)
+    console.log(typeof myAge)
+    console.log(typeof myHeight)
+    console.log(typeof isLearning)
+    console.log(typeof undefinedVar)
+    console.log(typeof nullVar)
+    console.log(typeof mySymbol)
+    console.log(typeof myBigNumber)
+
+    console.log(typeof nullVar)
+
+
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+    myName = "jonnanno"
+    myAge = 18
+    myHeight =1.80
+    isLearning = false
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
+    myName = 1
+    myAge = "18"
+    myHeight = true
+    isLearning = "si"
+
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
+ //   const myString = "hola"
+ //   const myNumber = 1
+ //   const myBoolean = true
+ //   const myUndefined = undefined
+ //   const myNull = null
+ //   const mySymbol = Symbol("mi simbolo")
+ //   const myBigNumber = BigInt(1n)
+
+
 // 9. A continuación, modifica los valores de las constantes
+
+ //   const myString = "adios"
+ //   const myNumber = "1"
+ //   const myBoolean = "true"
+ //   const myUndefined = "undefined2"
+ //   const myNull = "null2"
+ //   const mySymbol = Symbol("mi simbolo2")
+ //   const myBigNumber = BigInt("1n2")
+
+
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
